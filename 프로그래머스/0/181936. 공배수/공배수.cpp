146f0ac1c0978ca46dd9f23efd1158282c091007
wys0530 <166965895+wys0530@users.noreply.github.com>
2025-08-23 22:38:39 +0900

    [level 0] Title: 공배수, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.cpp
+++ 프로그래머스/0/181936. 공배수/공배수.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 
 int solution(int number, int n, int m) {
-    int answer = (number % n == 0 && number % m == 0)? 1 : 0; 
+    int answer;
+    if(number % n == 0 && number % m == 0)
+        answer= 1;
+    else
+        answer= 0; 
     return answer;
 }
