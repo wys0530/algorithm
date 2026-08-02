ccc3d746b703156acf3b32f118eba5f422b4aaeb
wys0530 <166965895+wys0530@users.noreply.github.com>
2026-08-03 00:23:33 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -3,9 +3,17 @@ class Solution
     public int solution(int n, int a, int b)
     {
         int answer;
-        for(answer=0; a !=b; answer++){
-            a = (a + 1) /2;
-            b = (b + 1) /2;
+        
+        for(answer = 0; a != b; answer++){
+            if(a%2==0)
+                a = a/2;
+            else 
+                a = (a+1)/2;
+            if(b%2==0)
+                b = b/2;
+            else
+                b = (b+1)/2;
+                
         }
 
         return answer;
